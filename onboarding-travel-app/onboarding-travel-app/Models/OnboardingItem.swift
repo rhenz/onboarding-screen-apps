2e8adefb63e8fa570fc8b73b2e811cf7e18f9503
rhenz <salvador.rhenz@gmail.com>
2022-08-26 22:17:06 +0800

    Add background images on `Onboarding Screens`

--- onboarding-travel-app/onboarding-travel-app/Models/OnboardingItem.swift
+++ onboarding-travel-app/onboarding-travel-app/Models/OnboardingItem.swift
@@ -19,16 +19,16 @@ extension OnboardingItem {
         return [
             OnboardingItem(title: "Travel Your Way",
                            detail: "Travel the world by air, rail or sea at the most competitive prices",
-                           bgImage: nil),
+                           bgImage: UIImage(named: "travel1")),
             OnboardingItem(title: "Stay Your Way",
                            detail: "With over millions of hotel worldwide, find the perfect accomodation in the most amazing places!",
-                           bgImage: nil),
+                           bgImage: UIImage(named: "travel2")),
             OnboardingItem(title: "Discover Your Way",
                            detail: "Explore exotic destinations with our new features that link you to like-minded travelers!",
-                           bgImage: nil),
+                           bgImage: UIImage(named: "travel3")),
             OnboardingItem(title: "Feast Your Way",
                            detail: "We recommend you local cuisines that are safe and highly recommended by the locals!",
-                           bgImage: nil)
+                           bgImage: UIImage(named: "travel4"))
         ]
     }
 }
