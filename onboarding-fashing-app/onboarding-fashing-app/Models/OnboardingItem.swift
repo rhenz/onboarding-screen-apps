3e89bf2c56a9cc30e33602ba7eb6840111036af1
rhenz <salvador.rhenz@gmail.com>
2022-08-30 14:03:04 +0800

    Fix some minor UI issues in the storyboard and the OnboardingItem model issue

--- onboarding-fashing-app/onboarding-fashing-app/Models/OnboardingItem.swift
+++ onboarding-fashing-app/onboarding-fashing-app/Models/OnboardingItem.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 struct OnboardingItem {
-    let authorName: String
     let authorQuote: String
+    let authorName: String
 }
 
 extension OnboardingItem {
     static func createSampleData() -> [Self] {
         return [
-            .init(authorName: "I like my money right where I can see it…hanging in my closet.", authorQuote: "Carrie Bradshaw"),
-            .init(authorName: "I think there is beauty in everything. What 'normal' people perceive as ugly, I can usually see something of beauty in it.", authorQuote: "Alexander McQueen"),
-            .init(authorName: "Style is something each of us already has, all we need to do is find it.", authorQuote: "Diane von Furstenberg"),
-            .init(authorName: "Fashion is the armor to survive the reality of everyday life.", authorQuote: "Bill Cunningham"),
-            .init(authorName: "I don't design clothes. I design dreams.", authorQuote: "Ralph Lauren")
+            .init(authorQuote: "I like my money right where I can see it…hanging in my closet.", authorName: "Carrie Bradshaw"),
+            .init(authorQuote: "I think there is beauty in everything. What 'normal' people perceive as ugly, I can usually see something of beauty in it.", authorName: "Alexander McQueen"),
+            .init(authorQuote: "Style is something each of us already has, all we need to do is find it.", authorName: "Diane von Furstenberg"),
+            .init(authorQuote: "Fashion is the armor to survive the reality of everyday life.", authorName: "Bill Cunningham"),
+            .init(authorQuote: "I don't design clothes. I design dreams.", authorName: "Ralph Lauren")
         ]
     }
 }
