f8245c99fd17f4823c1d6ccd37d6bd97bc5b6c22
rhenz <salvador.rhenz@gmail.com>
2022-09-03 11:45:33 +0800

    Add new scene in storyboard for MainViewController. Just a temporary screen to show later on

---
+++ onboarding-fashion-app/onboarding-fashion-app/View Controllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  onboarding-fashion-app
+//
+//  Created by John Salvador on 9/3/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
