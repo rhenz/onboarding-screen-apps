b86f40e3a8562e906aa34346a01ee7bce41eb8ab
rhenz <salvador.rhenz@gmail.com>
2022-09-05 18:06:25 +0800

    Setup the AVPlayer that will play the background video in Onboarding Screen

--- onboarding-holiday-app/onboarding-holiday-app/View Controllers/OnboardingViewController.swift
+++ onboarding-holiday-app/onboarding-holiday-app/View Controllers/OnboardingViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class OnboardingViewController: UIViewController {
 
@@ -16,24 +17,82 @@ class OnboardingViewController: UIViewController {
     
     override var prefersStatusBarHidden: Bool { true }
     
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
+        setupPlayerIfNeeded()
+        restartVideo()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
+}
 
+// MARK: - AVPlayer
 
+extension OnboardingViewController {
+    private func buildPlayer() -> AVPlayer? {
+        guard let filePath = Bundle.main.path(forResource: "bg_holiday", ofType: ".mp4") else {
+            return nil
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: filePath))
+        player.actionAtItemEnd = .none
+        player.isMuted = true
+        return player
+    }
+    
+    private func buildPlayerLayer() -> AVPlayerLayer? {
+        let layer = AVPlayerLayer(player: player)
+        layer.videoGravity = .resizeAspectFill
+        layer.frame = view.bounds
+        return layer
+    }
+    
+    private func playVideo() {
+        player?.play()
+    }
+    
+    private func restartVideo() {
+        player?.seek(to: .zero)
+        playVideo()
+    }
+    
+    private func pauseVideo() {
+        player?.pause()
+    }
+    
+    private func setupPlayerIfNeeded() {
+        player = buildPlayer()
+        playerLayer = buildPlayerLayer()
+        
+        guard let playerLayer = self.playerLayer,
+              viewDoesNotContainPlayerLayer
+        else {
+            return
+        }
+        
+        view.layer.insertSublayer(playerLayer, at: 0)
+    }
+    
+    private var viewDoesNotContainPlayerLayer: Bool {
+        if let playerLayer = self.playerLayer, let sublayers = view.layer.sublayers {
+            return !sublayers.contains(playerLayer)
+        } else {
+            return false
+        }
+    }
 }
 
 // MARK: - Actions
