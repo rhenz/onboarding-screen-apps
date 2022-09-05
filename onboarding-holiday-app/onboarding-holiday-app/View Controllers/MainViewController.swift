11b909982bc7fd89f6e323f66525fc941b4d2baf
rhenz <salvador.rhenz@gmail.com>
2022-09-05 15:46:12 +0800

    Create dummy view controller that will act as the Main Screen once onboarding is done

---
+++ onboarding-holiday-app/onboarding-holiday-app/View Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  onboarding-holiday-app
+//
+//  Created by John Salvador on 9/5/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
