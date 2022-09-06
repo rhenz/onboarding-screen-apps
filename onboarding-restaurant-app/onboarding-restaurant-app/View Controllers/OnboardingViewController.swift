a9f3b522300b8ab0d7182f274b612bfc85977907
rhenz <salvador.rhenz@gmail.com>
2022-09-06 15:19:05 +0800

    Setup reusable cell(cellId) from storyboard display dummy cells for now

--- onboarding-restaurant-app/onboarding-restaurant-app/View Controllers/OnboardingViewController.swift
+++ onboarding-restaurant-app/onboarding-restaurant-app/View Controllers/OnboardingViewController.swift
@@ -17,7 +17,7 @@ class OnboardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
 
 
@@ -40,11 +40,13 @@ extension OnboardingViewController {
 
 extension OnboardingViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
 
