a484b239be647311f90850b18b87e40b04478040
rhenz <salvador.rhenz@gmail.com>
2022-09-06 15:07:10 +0800

    Setup initial project folder structure

---
+++ onboarding-restaurant-app/onboarding-restaurant-app/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onboarding-restaurant-app
+//
+//  Created by John Salvador on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
