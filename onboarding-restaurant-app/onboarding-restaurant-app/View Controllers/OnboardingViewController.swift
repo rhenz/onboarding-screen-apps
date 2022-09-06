f17cd15387183786a2cf37d54e4d3431a8ff1a06
rhenz <salvador.rhenz@gmail.com>
2022-09-06 15:15:55 +0800

    Setup minimum requirement for collection view

--- onboarding-restaurant-app/onboarding-restaurant-app/View Controllers/OnboardingViewController.swift
+++ onboarding-restaurant-app/onboarding-restaurant-app/View Controllers/OnboardingViewController.swift
@@ -23,3 +23,33 @@ class OnboardingViewController: UIViewController {
 
 }
 
+// MARK: - Setup UI
+
+extension OnboardingViewController {
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+// MARK: - Collection View Datasource
+
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+// MARK: - Collection View Delegate
+
+extension OnboardingViewController: UICollectionViewDelegate {
+    
+}
